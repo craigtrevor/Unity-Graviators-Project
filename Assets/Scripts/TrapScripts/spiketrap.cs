83b877aa74fc2ca0b8249f8d2c9cabd15cccdf58
PIXEL\Connor <connor.mccaffrey@gmail.com>
2017-05-17 21:35:01 +1000

    Fixed multiplayer bug. Added Connor B's traps to the main project. Updated .gitignore file.

---
+++ Assets/Scripts/TrapScripts/spiketrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spiketrap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			print ("Ouch!");
+			Destroy (other.gameObject);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
