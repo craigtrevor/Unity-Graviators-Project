0747d753e5a6d29670bf8686cfe3af52895db0be
Alex Bicknell <carax95@hotmail.com>
2017-06-01 08:52:34 +1000

    Neatened up the gravity UI script

--- Assets/Scripts/UI Scripts/GravityCharge.cs
+++ Assets/Scripts/UI Scripts/GravityCharge.cs
@@ -17,165 +17,51 @@ public class GravityCharge : MonoBehaviour {
     public RawImage GravEmpty3;
     public RawImage GravEmpty4;
     
-    //public float gravityCharge;
-  
-    // Use this for initialization
-    void Start()
-    {
-      GameObject.Find("3PSController").GetComponent<GravityAxisScript>().gravityCharge = 10000;
-    }
-	// Update is called once per frame
-	void Update () {
-        UIGravUpdate();
+	public int gravCharge;
+
+    public void Update()
+	{
+		//set the variable here
+		//gravCharge = 
+
+		if (gravCharge < 2000) {
+			GravFull.gameObject.SetActive (false);
+			GravEmpty.gameObject.SetActive (true);
+		} else {
+			GravFull.gameObject.SetActive (true);
+			GravEmpty.gameObject.SetActive (false);
+		}
+
+		if (gravCharge < 4000) {
+			GravFull1.gameObject.SetActive (false);
+			GravEmpty1.gameObject.SetActive (true);
+		} else {
+			GravFull1.gameObject.SetActive (true);
+			GravEmpty1.gameObject.SetActive (false);
+		}
+
+		if (gravCharge < 6000) {
+			GravFull2.gameObject.SetActive (false);
+			GravEmpty2.gameObject.SetActive (true);
+		} else {
+			GravFull2.gameObject.SetActive (true);
+			GravEmpty2.gameObject.SetActive (false);
+		}
+
+		if (gravCharge < 8000) {
+			GravFull3.gameObject.SetActive (false);
+			GravEmpty3.gameObject.SetActive (true);
+		} else {
+			GravFull3.gameObject.SetActive (true);
+			GravEmpty3.gameObject.SetActive (false);
+		}
+
+		if (gravCharge == 10000) {
+			GravFull4.gameObject.SetActive (false);
+			GravEmpty4.gameObject.SetActive (true);
+		} else {
+			GravFull4.gameObject.SetActive (true);
+			GravEmpty4.gameObject.SetActive (false);
+		}
 	}
-    public void UIGravUpdate()
-    {
-      /*GravFull.gameObject.SetActive(true);
-        GravFull1.gameObject.SetActive(true);
-        GravFull2.gameObject.SetActive(true);
-        GravFull3.gameObject.SetActive(true);
-        GravFull4.gameObject.SetActive(true);*/
-
-        if (GetComponent<GravityAxisScript>().gravityCharge == 2000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(false);
-            GravFull2.gameObject.SetActive(false);
-            GravFull3.gameObject.SetActive(false);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(true);
-            GravEmpty2.gameObject.SetActive(true);
-            GravEmpty3.gameObject.SetActive(true);
-            GravEmpty4.gameObject.SetActive(true);
-
-        }
-        else if (GetComponent<GravityAxisScript>().gravityCharge <= 2000)
-        {
-            GravFull.gameObject.SetActive(false);
-            GravFull1.gameObject.SetActive(false);
-            GravFull2.gameObject.SetActive(false);
-            GravFull3.gameObject.SetActive(false);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(true);
-            GravEmpty1.gameObject.SetActive(true);
-            GravEmpty2.gameObject.SetActive(true);
-            GravEmpty3.gameObject.SetActive(true);
-            GravEmpty4.gameObject.SetActive(true);
-        }
-        if (GetComponent<GravityAxisScript>().gravityCharge == 4000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(true);
-            GravFull2.gameObject.SetActive(false);
-            GravFull3.gameObject.SetActive(false);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(false);
-            GravEmpty2.gameObject.SetActive(true);
-            GravEmpty3.gameObject.SetActive(true);
-            GravEmpty4.gameObject.SetActive(true);
-        }
-        else if (GetComponent<GravityAxisScript>().gravityCharge >= 2000 && GetComponent<GravityAxisScript>().gravityCharge <= 4000)
-        {
-            GravFull.gameObject.SetActive(false);
-            GravFull1.gameObject.SetActive(false);
-            GravFull2.gameObject.SetActive(false);
-            GravFull3.gameObject.SetActive(false);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(true);
-            GravEmpty1.gameObject.SetActive(true);
-            GravEmpty2.gameObject.SetActive(true);
-            GravEmpty3.gameObject.SetActive(true);
-            GravEmpty4.gameObject.SetActive(true);
-        }
-        if (GetComponent<GravityAxisScript>().gravityCharge == 6000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(true);
-            GravFull2.gameObject.SetActive(true);
-            GravFull3.gameObject.SetActive(false);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(false);
-            GravEmpty2.gameObject.SetActive(false);
-            GravEmpty3.gameObject.SetActive(true);
-            GravEmpty4.gameObject.SetActive(true);
-        }
-        else if (GetComponent<GravityAxisScript>().gravityCharge >= 4000 && GetComponent<GravityAxisScript>().gravityCharge <= 6000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(true);
-            GravFull2.gameObject.SetActive(false);
-            GravFull3.gameObject.SetActive(false);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(false);
-            GravEmpty2.gameObject.SetActive(true);
-            GravEmpty3.gameObject.SetActive(true);
-            GravEmpty4.gameObject.SetActive(true);
-        }
-        if (GetComponent<GravityAxisScript>().gravityCharge == 8000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(true);
-            GravFull2.gameObject.SetActive(true);
-            GravFull3.gameObject.SetActive(true);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(false);
-            GravEmpty2.gameObject.SetActive(false);
-            GravEmpty3.gameObject.SetActive(false);
-            GravEmpty4.gameObject.SetActive(true);
-        }
-        else if (GetComponent<GravityAxisScript>().gravityCharge >= 6000 && GetComponent<GravityAxisScript>().gravityCharge <= 8000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(true);
-            GravFull2.gameObject.SetActive(true);
-            GravFull3.gameObject.SetActive(false);
-            GravFull4.gameObject.SetActive(false);
-
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(false);
-            GravEmpty2.gameObject.SetActive(false);
-            GravEmpty3.gameObject.SetActive(true);
-            GravEmpty4.gameObject.SetActive(true);
-        }
-        if (GetComponent<GravityAxisScript>().gravityCharge == 10000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(true);
-            GravFull2.gameObject.SetActive(true);
-            GravFull3.gameObject.SetActive(true);
-            GravFull4.gameObject.SetActive(true);
-        
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(false);
-            GravEmpty2.gameObject.SetActive(false);
-            GravEmpty3.gameObject.SetActive(false);
-            GravEmpty4.gameObject.SetActive(false);
-        }
-        else if (GetComponent<GravityAxisScript>().gravityCharge >= 8000 && GetComponent<GravityAxisScript>().gravityCharge <= 10000)
-        {
-            GravFull.gameObject.SetActive(true);
-            GravFull1.gameObject.SetActive(true);
-            GravFull2.gameObject.SetActive(true);
-            GravFull3.gameObject.SetActive(true);
-            GravFull4.gameObject.SetActive(true);
-
-            GravEmpty.gameObject.SetActive(false);
-            GravEmpty1.gameObject.SetActive(false);
-            GravEmpty2.gameObject.SetActive(false);
-            GravEmpty3.gameObject.SetActive(false);
-            GravEmpty4.gameObject.SetActive(false);
-        }
-    }
 }
