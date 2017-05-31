e1ab886af66f68317fc3c80759b5c128bd597ccb
Connor McCaffrey <connor.mccaffrey@gmail.com>
2017-05-31 20:48:40 +1000

    Improved Player Camera. Minor changes to avoid clipping.

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Vector3 desiredLocalPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.desiredLocalPosition = this.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 desiredPosition = this.transform.parent.position + (this.transform.parent.rotation * this.desiredLocalPosition);
+        Vector3 playerPosition = this.transform.parent.position;
+        Vector3 dir = (desiredPosition - playerPosition).normalized;
+        float magnitude = (desiredPosition - playerPosition).magnitude;
+
+        if (Physics.Raycast(playerPosition, dir * magnitude, out hit, magnitude))
+        {
+            this.transform.position = Vector3.Lerp(hit.point, playerPosition, 0.2f);
+        }
+        else
+        {
+            this.transform.localPosition = this.desiredLocalPosition;
+        }
+    }
+}
