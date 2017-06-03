d41e3ab2be6fbdb2ff972087690df52ba0e0c9dd
Alex Bicknell <carax95@hotmail.com>
2017-06-03 18:25:50 +1000

    added NoNameCorpse prefab. KABOOM!

---
+++ Assets/Scripts/Animation Scripts/CorpseDissolve.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorpseDissolve : MonoBehaviour {
+
+	public Renderer[] materials;
+	public Rigidbody[] rbodies;
+	public bool dying;
+	public float deathCount = 0;
+
+	void Death (Vector3 direction) {
+		dying = true;
+		foreach (Rigidbody rbody in rbodies) {
+			rbody.AddForce (direction);
+		}
+	}
+
+	void Start () {
+		materials = GetComponentsInChildren<Renderer> ();
+		rbodies = GetComponentsInChildren<Rigidbody> ();
+
+		Death(new Vector3 (0,0,0));
+	}
+
+	void Update () {
+		if (dying) {
+			deathCount += Time.time * 0.001f;
+			foreach (Renderer mat in materials) {
+				mat.material.SetFloat ("_Cutoff", deathCount);
+			}
+		}
+		if (deathCount >= 1) {
+			Destroy (this.gameObject);
+		}
+	}
+}
