5ff373e37b293f992fb0d36247ea234ef973144a
ConnorB <mrconnorburton2@gmail.com>
2017-07-11 23:43:01 +1000

    # - Fixed annoying black panels on all the objects, should fix some camera clipping too. # - First attempt at a (bad) slowing zone. Couldn't get it to work. Will continue when I get the opportunity. # - Made some other small level changes. Keen on reworking the main level.

---
+++ Assets/Scripts/Trap Scripts/SlowTrap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTrap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float Forward = GetComponent<PlayerController> ().moveSettings.forwardVel;
+		float Right = GetComponent<PlayerController> ().moveSettings.rightVel;
+		float RotateVel = GetComponent<PlayerController> ().moveSettings.rotateVel;
+		float Jump = GetComponent<PlayerController> ().moveSettings.jumpVel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			print ("Hi");
+
+			//float Jump / 2;
+		}
+	}
+}
