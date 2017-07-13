63eeb604ee75c2f58563af6efc7eab9a713bf81d
PIXEL\Connor <connor.mccaffrey@gmail.com>
2017-07-13 22:20:31 +1000

    Network Optimizations. Better synced movement & rotation for players. Tested up to 6 players and didn't get too much lag. Fixed lighting error message. Had to upgrade the lighting assests to the latest Unity version.
    Future Update: Even better synced movement & rotation, less send rates to the server to decrease lag, add weapon trails and player soundscape back to the player prefab.

---
+++ Assets/Scripts/Network Scripts/Network_PlayerSyncPos.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Network_PlayerSyncPos : NetworkBehaviour {
+
+    [SyncVar]
+    private Vector3 syncPos;
+    private Vector3 lastPos;
+
+    [SerializeField]
+    Transform myTransform;
+
+    [SerializeField]
+    float lerpRate = 15;
+    private float threshold = 0.5f;
+
+    void FixedUpdate()
+    {
+        TransmitPosition();
+        LerpPosition();
+    }
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer (Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void TransmitPosition()
+    {
+        if (isLocalPlayer && Vector3.Distance(myTransform.position, lastPos) > threshold)
+        {
+            CmdProvidePositionToServer(myTransform.position);
+            lastPos = myTransform.position;
+        }
+    }
+}
