c6792611d13407c6eda3030c1dbb907393aa7f77
TIMS-PC\Timothy <tconw16@hotmail.com>
2017-07-26 15:52:54 +1000

    Added base Knock Back mecaninc, Needs testing

--- Assets/Scripts/Melee Scripts/Combat_Manager.cs
+++ Assets/Scripts/Melee Scripts/Combat_Manager.cs
@@ -14,8 +14,12 @@ public class Combat_Manager : NetworkBehaviour {
 
     private Collider[] hitColliders;
 
+	public Transform cameraRotation; // the camerasrotation, assign in game editor
+
     // Int
     public float playerDamage;
+	public int thrust = 1000; //change this for speed of knock back
+	public float delay = 5.00f; 
     //private int attackMask;
     private Vector3 attackOffset;
 
@@ -78,13 +82,35 @@ public class Combat_Manager : NetworkBehaviour {
         {
             if (hitCol.transform.root != transform.root && hitCol.gameObject.tag == PLAYER_TAG)
             {
-                Debug.Log("Hit Player!");
+				if (hitCol.GetComponent<Combat_Manager> ().playerDamage < this.GetComponent<Combat_Manager> ().playerDamage) { // checks to see if the other player has a smaller damage
+					Debug.Log ("Hit Player!");
+					Debug.Log (hitCol.GetComponent<Combat_Manager> ().playerDamage);
 
-                CmdTakeDamage(hitCol.gameObject.name, playerDamage, transform.name);
-                isAttacking = false;
+					CmdTakeDamage (hitCol.gameObject.name, playerDamage, transform.name);
+					isAttacking = false;
+				} else 
+				{
+					
+					StartCoroutine (knockBack ());
+				}
             }
         }
     }
+
+	IEnumerator knockBack()
+	{
+		Debug.Log ("knock Back");
+		GetComponentInChildren<PlayerController> ().enabled = false; // turn off player controls
+		playerRigidbody.constraints = RigidbodyConstraints.None; // allows the player to move around the 3 axis's
+		//playerRigidbody.constraints = RigidbodyConstraints.FreezeRotation; // stops the player from rotating
+		playerRigidbody.AddForce(cameraRotation.forward * -thrust);
+		yield return new WaitForSeconds (delay);
+		playerRigidbody.constraints = RigidbodyConstraints.None; // free the player to allow movement agian
+		playerRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+		GetComponentInChildren<PlayerController> ().enabled = true; // turn on player controls
+		//playerRigidBody.AddForce (cameraRotation.forward * thrust);// push the player forward
+		// get the palyer rigidbody and push it back
+	}
 
     [Command]
     void CmdTakeDamage(string _playerID, float _damage, string _sourceID)
