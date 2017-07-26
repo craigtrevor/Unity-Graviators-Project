0716efb47c2fe8fd343291f59c6ef20e8076b129
Connor McCaffrey <connor.mccaffrey@gmail.com>
2017-07-26 15:38:29 +1000

    Implemented WIP Character Select Screen. New button added on Lobby_Scene. Use this button to switch to the Character_Select scene.
    At the moment, there are no visual changes when creating a match but this will change once we add the character models for Sparkus & Unit-D1.

---
+++ Assets/Scripts/Network Scripts/Network_Manager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
+
+public class Network_Manager : NetworkManager {
+
+    private GameObject buttonUI;
+    public string characterName;
+
+    [SerializeField]
+    private Button[] characterButtonArray;
+
+    int characterIndex = 0;
+
+    void Start()
+    {
+        characterName = "Err:NoName";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        IntializeUI();
+    }
+
+    void IntializeUI()
+    {
+        if (Network_SceneManager.instance.sceneName == "Character_Select")
+        {
+            buttonUI = GameObject.FindGameObjectWithTag("UI");
+            characterButtonArray = buttonUI.GetComponentsInChildren<Button>();
+            AddListeners();
+        }
+
+        if (Network_SceneManager.instance.sceneName != "Character_Select")
+        {
+            Array.Clear(characterButtonArray, 0, characterButtonArray.Length);
+        }
+    }
+
+    void AddListeners()
+    {
+        characterButtonArray[0].onClick.AddListener(delegate { CharacterSelector(characterButtonArray[0].name); });
+        characterButtonArray[1].onClick.AddListener(delegate { CharacterSelector(characterButtonArray[1].name); });
+        characterButtonArray[2].onClick.AddListener(delegate { CharacterSelector(characterButtonArray[2].name); });
+    }
+
+    void CharacterSelector(string buttonName)
+    {
+        switch (buttonName)
+        {
+            case "ErrNoName_btn":
+                characterIndex = 0;
+                characterName = "Err:NoName";
+                break;
+
+            case "Sparkus_btn":
+                characterIndex = 1;
+                characterName = "Sparkus";
+                break;
+
+            case "UnitD1_btn":
+                characterIndex = 2;
+                characterName = "Unit-D1";
+                break;
+        }
+
+        playerPrefab = spawnPrefabs[characterIndex];
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+
+        IntegerMessage msg = new IntegerMessage(characterIndex);
+
+        if (!clientLoadedScene)
+        {
+            // Ready/AddPlayer is usually triggered by a scene load completing. if no scene was loaded, then Ready/AddPlayer it here instead.
+            ClientScene.Ready(conn);
+            if (autoCreatePlayer)
+            {
+                ClientScene.AddPlayer(conn, 0, msg);
+            }
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
+    {
+        int id = 0;
+
+        if (extraMessageReader != null)
+        {
+            IntegerMessage i = extraMessageReader.ReadMessage<IntegerMessage>();
+        }
+
+        GameObject playerPrefab = spawnPrefabs[id];
+
+        GameObject player;
+        Transform startPos = GetStartPosition();
+        if (startPos != null)
+        {
+            player = (GameObject)Instantiate(playerPrefab, startPos.position, startPos.rotation);
+        }
+        else
+        {
+            player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        }
+
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+
+}
--- Assets/Scripts/UI Scripts/UI_UserAccountLobby.cs
+++ Assets/Scripts/UI Scripts/UI_UserAccountLobby.cs
@@ -1,14 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UI_UserAccountLobby : MonoBehaviour {
 
     public Text usernameText;
+    public Text characterText;
+
+    [SerializeField]
+    Network_Manager networkManager;
+
 
     void Start()
     {
         if (UI_UserAccountManager.IsLoggedIn)
             usernameText.text = UI_UserAccountManager.LoggedIn_Username;
+
+        networkManager = GameObject.Find("CustomNetworkManager").GetComponent<Network_Manager>();
+
+        characterText.text = networkManager.characterName;
+
+    }
+
+    public void SwitchCharacter()
+    {
+        SceneManager.LoadScene("Character_Select");          
     }
 
     public void LogOut()
