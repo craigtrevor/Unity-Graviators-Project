45c62c10c01f5de5126378196dfa9981a1670c03
TIMS-PC\Timothy <tconw16@hotmail.com>
2017-07-27 16:49:25 +1000

    Sword throw now spawns the weapon correctly for all players, does damage to players(still buggy)

--- Assets/Scripts/Weapon Scripts/Melee/damageRange.cs
+++ Assets/Scripts/Weapon Scripts/Melee/damageRange.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class damageRange : MonoBehaviour {
+public class damageRange : NetworkBehaviour {
 	public int Damage = 5;
 	public float rotateSpeed = 500;
+
+	private Collider[] hitColliders;
+	private Vector3 attackOffset;
+	private float attackRadius;
+	private const string PLAYER_TAG = "Player";
 	// Use this for initialization
 	void Start () {
 	}
@@ -12,7 +18,7 @@ public class damageRange : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (Vector3.down, rotateSpeed * Time.deltaTime);
-		
+		ThrowDamage();
 	}
 
 	void OnTriggerEnter(Collider col)
@@ -21,5 +27,43 @@ public class damageRange : MonoBehaviour {
 			Debug.Log ("i have hit "+ col.tag+ " and am now dissapering");
 			Destroy (this.gameObject);
 		}
+	}
+
+	[Client]
+	public void ThrowDamage()
+	{
+		hitColliders = Physics.OverlapSphere(transform.TransformPoint(attackOffset), attackRadius);
+
+		foreach (Collider hitCol in hitColliders)
+		{
+			if (hitCol.transform.root != transform.root && hitCol.gameObject.tag == PLAYER_TAG)
+			{
+				Debug.Log("Hit Player!");
+
+				CmdTakeDamage(hitCol.gameObject.name, Damage, transform.name);
+
+				Destroy (this.gameObject);
+			}
+		}
+	}
+
+	[Command]
+	void CmdTakeDamage(string _playerID, float _damage, string _sourceID)
+	{
+		Debug.Log(_playerID + " has been attacked.");
+
+		Network_PlayerManager networkPlayerStats = Network_GameManager.GetPlayer(_playerID);
+
+		networkPlayerStats.RpcTakeDamage(_damage, _sourceID);
+	}
+
+	[Command]
+	protected void CmdPlayerAttacked(string _playerID, float _damage, string _sourceID)
+	{
+		Debug.Log(_playerID + " has been attacked.");
+
+		Network_PlayerManager networkPlayerStats = Network_GameManager.GetPlayer(_playerID);
+
+		networkPlayerStats.RpcTakeDamage(_damage, _sourceID);
 	}
 }
