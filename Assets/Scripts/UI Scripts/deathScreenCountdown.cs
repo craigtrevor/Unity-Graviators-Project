85032a484168df815b253ddc2096c52a9b3fba72
Indigo <blueisme97@hotmail.com>
2017-08-14 21:49:56 +1000

    Added sponsered death with working countdown, fiddled with ult bar (still not working), swapped out health regen pads and added particle effect

---
+++ Assets/Scripts/UI Scripts/deathScreenCountdown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deathScreenCountdown : MonoBehaviour {
+
+	float timeLeft = 5.00f;
+
+	public Text countdownText;
+
+
+
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		countdownText.text = "" + Mathf.Round(timeLeft);
+		if(timeLeft < 0)
+		{
+			countdownText.text = "Respawning";
+		}
+	}
+}
