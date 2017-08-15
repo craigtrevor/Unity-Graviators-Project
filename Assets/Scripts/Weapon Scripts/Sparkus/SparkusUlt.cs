e75e32e4a3e561358c30efecca55b1ef29cc4268
Rob Peredo <robfperedo@gmail.com>
2017-08-15 17:22:21 +1000

    -very- preliminary sparkus ult stuff

--- Assets/Scripts/Weapon Scripts/Sparkus/SparkusUlt.cs
+++ Assets/Scripts/Weapon Scripts/Sparkus/SparkusUlt.cs
@@ -5,7 +5,16 @@ using UnityEngine.Networking;
 
 public class SparkusUlt : NetworkBehaviour {
 
-    public GameObject electricBeam;
+    private string sourceID;
+
+    const float LASER_DAMAGE = 0.1f;
+
+    public GameObject look;
+
+    public GameObject laserParticle;
+    GameObject playLaser;
+
+    private const string PLAYER_TAG = "Player";
 
     // Use this for initialization
     void Start() {
@@ -14,9 +23,41 @@ public class SparkusUlt : NetworkBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.F)) {            
-            //ult stuff here
+        if (Input.GetKey(KeyCode.F)) {
+
+            playLaser = (GameObject)Instantiate(laserParticle, this.transform.position, Quaternion.Euler(this.transform.eulerAngles.x, this.transform.eulerAngles.y - 90f, this.transform.eulerAngles.z), this.transform) as GameObject;
+            playLaser.GetComponent<ParticleSystem>().Emit(1);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, Vector3.forward, 30f)) {
+                if (Physics.Raycast(transform.position, Vector3.forward, out hit)) {
+                    if (hit.transform.root != transform.root && hit.transform.gameObject.tag == PLAYER_TAG/* && hit.transform.name != sourceID*/) {
+                        Debug.Log("Hit Player!");
+
+                        CmdLaserDamage(hit.transform.gameObject.name, LASER_DAMAGE, sourceID);
+                    }
+                }
+            } else {
+                //print("No boop");
+            }
+        } else {
+            Destroy(playLaser);
         }
+    }
+
+    [Command]
+    void CmdLaserDamage(string _playerID, float _damage, string _sourceID) {
+        Debug.Log(_playerID + " has been attacked.");
+
+        Network_PlayerManager networkPlayerStats = Network_GameManager.GetPlayer(_playerID);
+
+        networkPlayerStats.RpcTakeDamage(_damage, _sourceID);
+    }
+
+    void hurt() {
 
     }
+
 }
+
