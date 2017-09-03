3ac286db69c61cd5d42a329a7f3d07c7f02c7041
Alex Bicknell <carax95@hotmail.com>
2017-09-03 21:01:30 +1000

    New menu buttons

---
+++ Assets/Scripts/Animation Scripts/ButtonAnimate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimate : MonoBehaviour {
+
+	public Image thisImage;
+
+	public Sprite normalButton;
+	public Sprite hoverButton;
+	public Sprite clickButton;
+
+	public void OnPointerClick () {
+		thisImage.sprite = clickButton;
+	}
+
+	public void OnPointerEnter () {
+		thisImage.sprite = hoverButton;
+	}
+
+	public void OnPointerExit () {
+		thisImage.sprite = normalButton;
+	}
+}
