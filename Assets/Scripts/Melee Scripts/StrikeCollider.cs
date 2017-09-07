ca0ab6eedfa468a0bb32cbffc5ba199360f77202
Alex Bicknell <carax95@hotmail.com>
2017-09-07 14:37:44 +1000

    Fxied a bug?

--- Assets/Scripts/Melee Scripts/StrikeCollider.cs
+++ Assets/Scripts/Melee Scripts/StrikeCollider.cs
@@ -20,9 +20,9 @@ public class StrikeCollider : MonoBehaviour {
 	}
 		
 	void OnTriggerEnter(Collider other) {
-			if (other.transform.root.gameObject != this.transform.root.gameObject) {
-				hitPoint = thisCollider.ClosestPointOnBounds (other.transform.position);
-				combatManager.weaponCollide (other, hitPoint, airStrike);	
-			}
+		if (other.transform.root.gameObject != this.transform.root.gameObject && other.gameObject.tag != "ThrowingSword" && other.gameObject.tag != "Sparkus_RangedWeapon" || other.gameObject.tag != "UnitD1_RangedWeapon"))) {
+			hitPoint = thisCollider.ClosestPointOnBounds (other.transform.position);
+			combatManager.weaponCollide (other, hitPoint, airStrike);	
 		}
+	}
 }
