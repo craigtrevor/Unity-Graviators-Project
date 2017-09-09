868e27c1b3b412c73e2cd7f64893e4f50870c7ed
Alex Bicknell <carax95@hotmail.com>
2017-09-09 18:43:37 +1000

    Tesla Array added

---
+++ Assets/Scripts/Animation Scripts/TeslaCoilScrollUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeslaCoilScrollUp : MonoBehaviour {
+
+	public Renderer rend;
+	public Renderer rend2;
+	public float scroll;
+
+	void Update () {
+		scroll -= Time.deltaTime;
+		rend.material.SetTextureOffset ("_MainTex", new Vector2(0, scroll));
+		rend2.material.SetTextureOffset ("_MainTex", new Vector2(0, scroll - Time.deltaTime));
+	}
+}
---
+++ Assets/Scripts/Trap Scripts/TeslaCoil.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class TeslaCoil : NetworkBehaviour {
+
+	private string sourceID;
+
+	public GameObject StartCoil;
+	public GameObject EndCoil;
+	public LineRenderer lineRend;
+	public LightningLine lightningLine;
+	public BoxCollider boxCollider;
+	public bool Shocking;
+	public bool damageTick;
+	public List<GameObject> affectedList = new List<GameObject> ();
+
+	void SetInitialReferences(string _sourceID)
+	{
+		sourceID = _sourceID;
+	}
+		
+	void Start () {
+		lightningLine.target = StartCoil.transform.localPosition;
+		boxCollider.enabled = false;
+	}
+
+	void Update () {
+		if (!Shocking) {
+			StartCoroutine(Shock());
+		}
+		if (damageTick) {
+			StartCoroutine (SlowDamage ());
+		}
+	}
+
+	IEnumerator Shock() {
+		Shocking = true;
+		damageTick = true;
+		yield return new WaitForSeconds (Random.Range(3f,10f));
+		lightningLine.target = EndCoil.transform.localPosition;
+		boxCollider.enabled = true;
+		yield return new WaitForSeconds (5f);
+		Shocking = false;
+		damageTick = false;
+		lightningLine.target = StartCoil.transform.localPosition;
+		boxCollider.enabled = false;
+		affectedList.Clear ();
+		StopAllCoroutines ();
+	}
+		
+	public bool NameInList(GameObject toCheck) {
+		for (int i = 0; i < affectedList.Count; i++) {
+			if (affectedList [i] == toCheck) {
+				return true;
+			}
+		} 
+		return false;
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Player") {
+			if (!NameInList (collider.gameObject)) {
+				Debug.Log ("added");
+				affectedList.Add (collider.gameObject);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (collider.gameObject.tag == "Player") {
+			if (NameInList (collider.gameObject)) {
+				Debug.Log ("removed");
+				affectedList.Remove (collider.gameObject);
+			}
+		}
+	}
+
+	IEnumerator SlowDamage() {
+		damageTick = false;
+		yield return new WaitForSeconds (0.2f);
+		damageTick = true;
+		for (int i = 0; i < affectedList.Count; i++) {
+			CmdTakeDamage (affectedList[i].name, 5, sourceID);
+		}
+	}
+
+	[Command]
+	void CmdTakeDamage(string _playerID, float _damage, string _sourceID)
+	{
+		Network_PlayerManager networkPlayerStats = Network_GameManager.GetPlayer(_playerID);
+		networkPlayerStats.RpcTakeTrapDamage(_damage, _sourceID);
+	}
+}
