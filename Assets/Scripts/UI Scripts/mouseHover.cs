50ceadec724241ae5c37dd596a133249116fdb6c
Indigo <blueisme97@hotmail.com>
2017-09-17 22:51:16 +1000

    Combat balance tweaks, added character bios and fixed character select button positions (still working on scripting for this so currently disabled)

---
+++ Assets/Scripts/UI Scripts/mouseHover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class mouseHover: IPointerEnterHandler {
+	public GameObject characterBio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		characterBio.SetActive (true);
+	}
+}
