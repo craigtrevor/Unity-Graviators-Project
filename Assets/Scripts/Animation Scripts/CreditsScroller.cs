38df0198e83ec573967bcc237f9a3c47e4fa5d18
Alex Bicknell <carax95@hotmail.com>
2017-09-30 17:26:25 +1000

    Credits scene added

---
+++ Assets/Scripts/Animation Scripts/CreditsScroller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScroller : MonoBehaviour {
+
+	public RectTransform rectTransform;
+	public float speed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		rectTransform.position += new Vector3 (0, speed * Time.deltaTime, 0);
+	}
+}
