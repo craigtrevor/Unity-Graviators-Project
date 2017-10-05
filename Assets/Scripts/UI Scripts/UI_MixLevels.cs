06659daf868b257dea16a4cb1c993fba71a50d75
Connor McCaffrey <connor.mccaffrey@gmail.com>
2017-10-05 23:04:33 +1000

    Started to add some functionality to the Game_Settings scene. Added UI_MixLevels script. Atm, any changes to the Game_Settings scene won't save when leaving that scene but the volume on the audio mixers do change. Mute buttons don't work yet.

---
+++ Assets/Scripts/UI Scripts/UI_MixLevels.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class UI_MixLevels : MonoBehaviour
+{
+    [SerializeField]
+    AudioMixerGroup masterMixer;
+
+    [SerializeField]
+    AudioMixerGroup musicMixer;
+
+    [SerializeField]
+    AudioMixerGroup narrationMixer;
+
+    [SerializeField]
+    AudioMixerGroup combatMixer;
+
+    [SerializeField]
+    AudioMixerGroup atmosMixer;
+
+    [SerializeField]
+    AudioMixerGroup uiMixer;
+
+    public void SetMasterLevel(float masterLvl)
+    {
+        masterMixer.audioMixer.SetFloat("MasterVolume", masterLvl);
+    }
+
+    public void SetMusicLevel(float musicLvl)
+    {
+        musicMixer.audioMixer.SetFloat("MusicVolume", musicLvl);
+    }
+
+    public void SetNarrationLevel(float narrationLvl)
+    {
+        narrationMixer.audioMixer.SetFloat("NarrationVolume", narrationLvl);
+    }
+
+    public void SetCombatLevel(float combatLvl)
+    {
+        combatMixer.audioMixer.SetFloat("CombatVolume", combatLvl);
+    }
+
+    public void SetAtmosLevel(float atmosLvl)
+    {
+        atmosMixer.audioMixer.SetFloat("AtmosVolume", atmosLvl);
+    }
+
+    public void SetUILevel(float uiLvl)
+    {
+        uiMixer.audioMixer.SetFloat("UIVolume", uiLvl);
+    }
+}
